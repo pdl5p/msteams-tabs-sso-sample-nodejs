50be09549d07acfff35950e7934bdbe2d7c3dec6
Paul Lucas <paul.lucas@fivep.com.au>
2020-08-22 11:38:21 +1000

    tabs

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ var express = require('express');
 var app = express();
 
 // Add the route for handling tabs
-// var tabs = require('./tabs');
-// tabs.setup(app);
+var tabs = require('./tabs');
+tabs.setup(app);
+
 var port = process.env.PORT || 3333;
 
 
-app.get('/', function(req, res) {
-    res.send(`hello`);
-});
+// app.get('/', function(req, res) {
+//     res.send(`hello`);
+// });
 
 
 // Listen for incoming requests
